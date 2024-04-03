658749f72a2009fec9c8bfe459d6a0d2bd993ca8
Merenda Franco N <merenda.franco83@gmail.com>
2024-04-03 15:29:38 +0200

    Clean unused code

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 #define MATRIX_SIZE 512
 #define REPORT_DATA_SIZE 7
 
-void dot_product_V1();
-
 int main() {
     double **matrix_A = alloc_matrix_2D(MATRIX_SIZE);
     double **matrix_B = alloc_matrix_2D(MATRIX_SIZE);
@@ -27,24 +25,3 @@ int main() {
     dump_report_data_to_csv(time_data_reports, REPORT_DATA_SIZE);
     return 0;
 }
-
-
-
-
-
-//
-//void dot_product_V1() {
-//    struct timeval start, end;
-//    gettimeofday(&start, NULL);
-//
-//#pragma omp parallel for num_threads(2)
-//    for (int i = 0; i < MATRIX_SIZE; i++) {
-//        for (int k = 0; k < MATRIX_SIZE; k++) {
-//            for (int j = 0; j < MATRIX_SIZE; j++) {
-//                C[i][j] += A[i][k] * B[k][j];
-//            }
-//        }
-//    }
-//    gettimeofday(&end, NULL);
-//    printf("%0.6f\n", tdiff(&start, &end));
-//}
